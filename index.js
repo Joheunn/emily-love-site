7b3dffb165e39a0ef6505abdc2a23182ec9745d0
John Perez <johnperez@Johns-MacBook-Pro-2.local>
2025-05-24 22:28:49 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1 = ["a","b","c"], word2 = ["p","q","r"]) {
+    console.log(mergeAlternately);
+};
